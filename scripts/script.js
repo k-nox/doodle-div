4c767eaca0b224b43e4a5c6e16dd7d6019a7c3df
knox <lux.barker13@gmail.com>
2021-02-01 19:29:59 -0800

    Rearrange functions for readability

--- scripts/script.js
+++ scripts/script.js
@@ -2,23 +2,62 @@ const container = document.querySelector('.container');
 const buttons = document.querySelectorAll('button');
 const pickerInput = document.querySelector('#color-picker');
 
-// TODO: allow user to select grid size from a slider instead of a prompt
-
 let currentMode = 'black';
 let gridItems;
 let currentRainbowColor = 'violet';
 
-//* random hex color:
-const randomColor = function generateRandomHexColor(element) {
-  const div = element;
+const setMode = function setCurrentMode(color) {
+  currentMode = color;
+};
+
+//* creates grid from user input; defaults to 16x16
+const createGrid = function createGridFromUserInput(divsPerSide = 16) {
+  let totalDivs;
+  if (divsPerSide >= 100) {
+    totalDivs = 10000;
+  } else {
+    totalDivs = divsPerSide * divsPerSide;
+  }
+  for (let i = 0; i < totalDivs; i += 1) {
+    const div = document.createElement('div');
+    div.classList.add('grid-item');
+    container.style.setProperty('--divs-per-side', divsPerSide);
+    container.append(div);
+    gridItems = document.querySelectorAll('.grid-item');
+  }
+};
+
+//* enables hover functionality; defaults to black
+function changeColorOnHover() {
+  gridItems.forEach(item => {
+    item.addEventListener('mouseenter', function updateMode() {
+      changeColor(this);
+    });
+  });
+}
+
+function changeColor(item) {
+  const div = item;
+  if (!container.classList.contains('disabled')) {
+    if (typeof currentMode === 'function') {
+      currentMode(div);
+    } else {
+      div.style.background = currentMode;
+    }
+  }
+}
+
+//* generates random hex color
+function randomColor(item) {
+  const div = item;
   div.style.background = `#${Math.floor(Math.random() * 16777215).toString(
     16
   )}`;
-};
+}
 
 //* sets color in order of rainbow
-const rainbowColor = function generateColorsInRainbowOrder(element) {
-  const div = element;
+const rainbowColor = function generateColorsInRainbowOrder(item) {
+  const div = item;
   const rainbowArray = [
     'red',
     'orange',
@@ -39,16 +78,9 @@ const rainbowColor = function generateColorsInRainbowOrder(element) {
   div.style.background = currentRainbowColor;
 };
 
-//* erases all color and resets brightness level
-const erase = function eraseColorAndBrightness(element) {
-  const div = element;
-  div.style.background = 'white';
-  div.style.setProperty('--brightness-level', 1);
-};
-
 //* darkens grid item by 10% on each pass
-const darkenGridItem = function darkenGridItemGradually(element) {
-  const div = element;
+const darkenGridItem = function darkenGridItemGradually(item) {
+  const div = item;
   const currentBrightness = +getComputedStyle(div).getPropertyValue(
     '--brightness-level'
   );
@@ -59,12 +91,11 @@ const darkenGridItem = function darkenGridItemGradually(element) {
 };
 
 //* lightens grid item by 10% on each pass
-const lightenGridItem = function lightenGridItemGradually(element) {
-  const div = element;
+const lightenGridItem = function lightenGridItemGradually(item) {
+  const div = item;
   const currentBackgroundColor = getComputedStyle(div).getPropertyValue(
     'background-color'
   );
-
   // if current color is black, set to white with 0% brightness
   // black cannot be lightened with brightness
   if (currentBackgroundColor === 'rgb(0, 0, 0)') {
@@ -80,60 +111,11 @@ const lightenGridItem = function lightenGridItemGradually(element) {
   div.style.setProperty('--brightness-level', nextBrightness);
 };
 
-//* creates grid from user input; defaults to 16x16
-const createGrid = function createGridFromUserInput(divsPerSide = 16) {
-  let totalDivs;
-  if (divsPerSide >= 100) {
-    totalDivs = 10000;
-  } else {
-    totalDivs = divsPerSide * divsPerSide;
-  }
-  for (let i = 0; i < totalDivs; i += 1) {
-    const div = document.createElement('div');
-    div.classList.add('grid-item');
-    container.style.setProperty('--divs-per-side', divsPerSide);
-    container.append(div);
-    gridItems = document.querySelectorAll('.grid-item');
-  }
-};
-
-const changeColor = function changeDivColorOnHover(element) {
-  const div = element;
-  if (!container.classList.contains('disabled')) {
-    if (typeof currentMode === 'function') {
-      currentMode(div);
-    } else {
-      div.style.background = currentMode;
-    }
-  }
-};
-
-//* enables hover functionality; defaults to black
-const hover = function changeColorOnHover() {
-  gridItems.forEach(item => {
-    item.addEventListener('mouseenter', function update() {
-      changeColor(this);
-    });
-  });
-};
-
-const askUser = function askUserForDivs() {
-  // eslint-disable-next-line no-alert
-  const divs = prompt('How many squares should each side of the grid take up?');
-  if (!divs) {
-    return 16;
-  }
-  return divs;
-};
-
-const clearBoard = function resetAllDivsToBlank() {
-  gridItems.forEach(item => item.remove());
-};
-
-//* check if gridItems contain the grid-lines class
-const checkGrid = function checkIfGridLinesAreEnabled() {
-  const gridItemArray = Array.from(gridItems);
-  return gridItemArray.every(item => item.classList.contains('grid-lines'));
+//* erases all color and resets brightness level on hover
+const erase = function eraseColorAndBrightness(item) {
+  const div = item;
+  div.style.background = 'white';
+  div.style.setProperty('--brightness-level', 1);
 };
 
 //* clears board and resets grid to user-defined size on button click
@@ -144,19 +126,33 @@ const clear = function clearEverything() {
   if (currentGridLineStatus) {
     gridItems.forEach(item => item.classList.add('grid-lines'));
   }
-  hover();
+  changeColorOnHover();
 };
 
-const setMode = function setCurrentMode(color) {
-  currentMode = color;
-};
+function checkGrid() {
+  const gridItemArray = Array.from(gridItems);
+  return gridItemArray.every(item => item.classList.contains('grid-lines'));
+}
 
-pickerInput.addEventListener('change', e => setMode(e.target.value));
+function clearBoard() {
+  gridItems.forEach(item => item.remove());
+}
+
+function askUser() {
+  // eslint-disable-next-line no-alert
+  const divs = prompt('How many squares should each side of the grid take up?');
+  if (!divs) {
+    return 16;
+  }
+  return divs;
+}
 
 //* toggles drawing capability with click on container
 container.addEventListener('click', () => {
   container.classList.toggle('disabled');
 });
+
+pickerInput.addEventListener('change', e => setMode(e.target.value));
 
 buttons.forEach(button =>
   button.addEventListener('click', e => {
@@ -164,8 +160,9 @@ buttons.forEach(button =>
       case 'black':
         setMode('black');
         break;
-      case 'erase':
-        setMode(erase);
+      case 'pick':
+        pickerInput.focus();
+        pickerInput.click();
         break;
       case 'random':
         setMode(randomColor);
@@ -179,9 +176,8 @@ buttons.forEach(button =>
       case 'lighten':
         setMode(lightenGridItem);
         break;
-      case 'pick':
-        pickerInput.focus();
-        pickerInput.click();
+      case 'erase':
+        setMode(erase);
         break;
       case 'clear':
         clear();
@@ -195,8 +191,17 @@ buttons.forEach(button =>
   })
 );
 
+/* //* enables hover functionality; defaults to black
+function changeColorOnHover() {
+  gridItems.forEach(item => {
+    item.addEventListener('mouseenter', function updateMode() {
+      changeColor(this);
+    });
+  });
+} */
+
 document.addEventListener('DOMContentLoaded', () => {
   createGrid();
   gridItems.forEach(item => item.classList.add('grid-lines'));
-  hover();
+  changeColorOnHover();
 });
